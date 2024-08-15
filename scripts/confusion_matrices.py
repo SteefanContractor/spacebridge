26b2203bd6f4249ece4d3e00a80ed493c73e992e
SteefanContractor <stefan.contractor@gmail.com>
2024-08-15 11:21:02 +1000

    new script to print confusion matrices for lgbm and rmda; with and without umap; annually and seasonally

--- scripts/confusion_matrices.py
+++ scripts/confusion_matrices.py
@@ -111,12 +111,11 @@ def print_confusion_matrices(label, lgbm_pred, rmda_pred, lab_names=lab_names):
     print(confusion_matrix(label, rmda_pred, normalize='pred', labels=lab_names))
     print('-'*20)
     print()
-    print()
     print('-'*20)
     print('ice classes only')
     print('-'*20)
     print('true normalized')
-    print(lab_names)
+    print(['YI_conc','FYI_conc','MYI_conc'])
     print('LGBM confusion matrix')
     print(confusion_matrix(label, lgbm_pred, normalize='true', labels=['YI_conc','FYI_conc','MYI_conc']))
     print('RMDA confusion matrix')
@@ -129,6 +128,8 @@ def print_confusion_matrices(label, lgbm_pred, rmda_pred, lab_names=lab_names):
     print('RMDA confusion matrix')
     print(confusion_matrix(label, rmda_pred, normalize='pred', labels=['YI_conc','FYI_conc','MYI_conc']))
     print('-'*20)
+    print()
+    print()
 # %%
 # ### Training
 print_confusion_matrices(train_label, train_lgbm_pred, train_rmda_pred)
@@ -139,6 +140,152 @@ print_confusion_matrices(val_label, val_lgbm_pred, val_rmda_pred)
 # ### Testing
 print_confusion_matrices(test_label, test_lgbm_pred, test_rmda_pred)
 # %%
+# save the RMDA test confusion matrix
+test_cm = confusion_matrix(test_label, test_rmda_pred, normalize='true', labels=lab_names)
+with open('../products/models/rmda/test_cm_rmda_allfeats_K4C4_trust-constr_20240512:012533.pkl', 'wb') as f:
+    pickle.dump(test_cm, f)
+# %%
 # ## Seasonal
 # %%
-# summer
+# summer (JFM)
+# load metadata
+metadata = pd.read_csv(data_path + "metadata.csv", index_col=0, parse_dates=['date'])
+metadata = metadata.loc[labels.index]
+winter = metadata.query('20200801 <= date <= 20201031')
+summer = metadata.query('20200101 <= date <= 20200331') #date < 20200801 or date >= 20201101
+shoulder = metadata.drop(pd.concat([winter, summer]).index)
+assert len(winter)+len(summer)+len(shoulder) == len(metadata)
+# %%
+# filter train, val and test labels and feats by season
+def filter_by_season(season, labels, lgbm_pred, rmda_pred):
+    label_pred_df = pd.DataFrame({'label': labels, 'lgbm_pred': lgbm_pred, 'rmda_pred': rmda_pred}, index=labels.index)
+    label_pred_df = label_pred_df.join(season, how='inner')[label_pred_df.columns]
+    return label_pred_df.label, label_pred_df.lgbm_pred, label_pred_df.rmda_pred
+# %%
+# ### Training
+print('SUMMER')
+print('_'*20)
+print(filter_by_season(summer, train_label, train_lgbm_pred, train_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(summer, train_label, train_lgbm_pred, train_rmda_pred))
+print('WINTER')
+print('_'*20)
+print(filter_by_season(winter, train_label, train_lgbm_pred, train_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(winter, train_label, train_lgbm_pred, train_rmda_pred))
+print('SHOULDER')
+print('_'*20)
+print(filter_by_season(shoulder, train_label, train_lgbm_pred, train_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(shoulder, train_label, train_lgbm_pred, train_rmda_pred))
+# %%
+# ### Validation
+print('SUMMER')
+print('_'*20)
+print(filter_by_season(summer, val_label, val_lgbm_pred, val_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(summer, val_label, val_lgbm_pred, val_rmda_pred))
+print('WINTER')
+print('_'*20)
+print(filter_by_season(winter, val_label, val_lgbm_pred, val_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(winter, val_label, val_lgbm_pred, val_rmda_pred))
+print('SHOULDER')
+print('_'*20)
+print(filter_by_season(shoulder, val_label, val_lgbm_pred, val_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(shoulder, val_label, val_lgbm_pred, val_rmda_pred))
+# %%
+# ### Testing
+# ### Validation
+print('SUMMER')
+print('_'*20)
+print(filter_by_season(summer, test_label, test_lgbm_pred, test_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(summer, test_label, test_lgbm_pred, test_rmda_pred))
+print('WINTER')
+print('_'*20)
+print(filter_by_season(winter, test_label, test_lgbm_pred, test_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(winter, test_label, test_lgbm_pred, test_rmda_pred))
+print('SHOULDER')
+print('_'*20)
+print(filter_by_season(shoulder, test_label, test_lgbm_pred, test_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(shoulder, test_label, test_lgbm_pred, test_rmda_pred))
+# %%
+# # With UMAP
+# ## Annual
+# %%
+# load sk_bgmm model of rmda model
+with open('../products/models/sk_bgmm/bgmm_4comp_1000iter_SMOTERUS12pc_umap.pkl','rb') as f:
+    bgmm_model = pickle.load(f)
+# load lgbm model
+with open('../products/models/train_gradboost/lgbm_clf_4class_SMOTERUS12pc_umap_20240514:202900.pkl', 'rb') as f:
+    lgbm_model = pickle.load(f)
+# %%
+with open('../products/models/rmda/rmda_umapfeats_K4C4_trust-constr_20240604:171926.pkl', 'rb') as f:
+        result = pickle.load(f)
+R = result.x
+R = np.exp(R)
+R = R.reshape((4,4))
+R = R/R.sum(axis=0)
+print(R)
+# %%
+# load umap transformation
+with open('../products/models/umap_transformation/umap_1200Kresampledfeats_mindist0.5_neighbors75_numcomp5_20240514:111207.pkl', "rb") as f:
+    umap = pickle.load(f)
+# %%
+print('training prediction')
+train_lgbm_pred, train_rmda_pred = predict_labs(umap.transform(train_features))
+print('validation prediction')
+val_lgbm_pred, val_rmda_pred = predict_labs(umap.transform(val_feats))
+print('test prediction')
+test_lgbm_pred, test_rmda_pred = predict_labs(umap.transform(test_features))
+# %%
+# ### Training
+print_confusion_matrices(train_label, train_lgbm_pred, train_rmda_pred)
+# %%
+# ### Validation
+print_confusion_matrices(val_label, val_lgbm_pred, val_rmda_pred)
+# %%
+# ### Testing
+print_confusion_matrices(test_label, test_lgbm_pred, test_rmda_pred)
+# save the RMDA test confusion matrix
+test_cm = confusion_matrix(test_label, test_rmda_pred, normalize='true', labels=lab_names)
+with open('../products/models/rmda/test_cm_rmda_umapfeats_K4C4_trust-constr_20240604:171926.pkl', 'wb') as f:
+    pickle.dump(test_cm, f)
+# %%
+# ### Training
+print('SUMMER')
+print('_'*20)
+print(filter_by_season(summer, train_label, train_lgbm_pred, train_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(summer, train_label, train_lgbm_pred, train_rmda_pred))
+print('WINTER')
+print('_'*20)
+print(filter_by_season(winter, train_label, train_lgbm_pred, train_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(winter, train_label, train_lgbm_pred, train_rmda_pred))
+print('SHOULDER')
+print('_'*20)
+print(filter_by_season(shoulder, train_label, train_lgbm_pred, train_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(shoulder, train_label, train_lgbm_pred, train_rmda_pred))
+# %%
+# ### Validation
+print('SUMMER')
+print('_'*20)
+print(filter_by_season(summer, val_label, val_lgbm_pred, val_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(summer, val_label, val_lgbm_pred, val_rmda_pred))
+print('WINTER')
+print('_'*20)
+print(filter_by_season(winter, val_label, val_lgbm_pred, val_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(winter, val_label, val_lgbm_pred, val_rmda_pred))
+print('SHOULDER')
+print('_'*20)
+print(filter_by_season(shoulder, val_label, val_lgbm_pred, val_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(shoulder, val_label, val_lgbm_pred, val_rmda_pred))
+# %%
+# ### Testing
+# ### Validation
+print('SUMMER')
+print('_'*20)
+print(filter_by_season(summer, test_label, test_lgbm_pred, test_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(summer, test_label, test_lgbm_pred, test_rmda_pred))
+print('WINTER')
+print('_'*20)
+print(filter_by_season(winter, test_label, test_lgbm_pred, test_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(winter, test_label, test_lgbm_pred, test_rmda_pred))
+print('SHOULDER')
+print('_'*20)
+print(filter_by_season(shoulder, test_label, test_lgbm_pred, test_rmda_pred)[0].value_counts())
+print_confusion_matrices(*filter_by_season(shoulder, test_label, test_lgbm_pred, test_rmda_pred))
